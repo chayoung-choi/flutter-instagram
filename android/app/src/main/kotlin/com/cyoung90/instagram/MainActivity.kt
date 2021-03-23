05b0b1fdc54e02752e3198cc3f682c4ec53d50a2
chayoung-choi <chayoung.dev@gmail.com>
2021-03-23 17:30:56 +0900

    flutter init

---
+++ android/app/src/main/kotlin/com/cyoung90/instagram/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.cyoung90.instagram
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/feed_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class FeedScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          'instagram',
+          style: TextStyle(fontFamily: 'VeganStyle', color: Colors.black87),
+        )
+      ),
+    );
+  }
+}
