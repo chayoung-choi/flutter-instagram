06c856ddef82c5facb1a191fb7b01df844d897d8
chayoung-choi <chayoung.dev@gmail.com>
2021-03-24 17:29:41 +0900

    post 위젯 분리

---
+++ lib/widget/post.dart
@@ -0,0 +1,45 @@
+import 'package:cached_network_image/cached_network_image.dart';
+import 'package:flutter/material.dart';
+
+class Post extends StatelessWidget {
+  final int index;
+  Size size;
+
+  Post(
+    this.index, {
+    Key key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    if (size == null) {
+      size = MediaQuery.of(context).size; // device size
+    }
+
+    return CachedNetworkImage(
+      imageUrl: 'https://picsum.photos/id/$index/2000/2000',
+      placeholder: (BuildContext context, String url) {
+        return Container(
+            width: size.width,
+            height: size.width,
+            child: Center(
+                child: SizedBox(
+                    height: 60,
+                    width: 60,
+                    child: CircularProgressIndicator(
+                      backgroundColor: Colors.black87,
+                    ))));
+      },
+      imageBuilder: (BuildContext context, ImageProvider imageProvider) {
+        return AspectRatio(
+          aspectRatio: 1,
+          child: Container(
+            decoration: BoxDecoration(
+                image:
+                    DecorationImage(image: imageProvider, fit: BoxFit.cover)),
+          ),
+        );
+      },
+    );
+  }
+}
