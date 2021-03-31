c9cb8783eb265ed9c691e1090b6f736b3f3257d9
chayoung-choi <chayoung.dev@gmail.com>
2021-03-31 18:00:48 +0900

    Profile Screen Layout  - profile_side_menu

--- lib/screens/profile_screen.dart
+++ lib/screens/profile_screen.dart
@@ -1,42 +1,60 @@
 import 'package:flutter/material.dart';
+import 'package:instagram/constants/screen_size.dart';
 import 'package:instagram/widget/profile_body.dart';
+import 'package:instagram/widget/profile_side_menu.dart';
 
-class ProfileScreen extends StatelessWidget {
+const duration = Duration(milliseconds: 1000);
+
+class ProfileScreen extends StatefulWidget {
+  @override
+  _ProfileScreenState createState() => _ProfileScreenState();
+}
+
+class _ProfileScreenState extends State<ProfileScreen> {
+  final menuWidth = size.width / 3 * 2;
+
+  MenuStatus _menuStatus = MenuStatus.closed;
+  double bodyXPos = 0;
+  double menuXPos = size.width;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.grey[100],
-      body: SafeArea(
-        // 상단 상태바 밑으로 body
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            _appbar(),
-            ProfileBody(),
-          ],
-        ),
+      body: Stack(
+        children: [
+          AnimatedContainer(
+            duration: duration,
+            curve: Curves.fastOutSlowIn,
+            child: ProfileBody(onMenuChanged: () {
+              setState(() {
+                _menuStatus = (_menuStatus == MenuStatus.closed)
+                    ? MenuStatus.opened
+                    : MenuStatus.closed;
+                switch (_menuStatus) {
+                  case MenuStatus.opened:
+                    bodyXPos = -menuWidth;
+                    menuXPos = size.width - menuWidth;
+                    break;
+                  case MenuStatus.closed:
+                    bodyXPos = 0;
+                    menuXPos = size.width;
+                    break;
+                }
+              });
+            }),
+            transform: Matrix4.translationValues(bodyXPos, 0, 0),
+          ),
+          AnimatedContainer(
+            duration: duration,
+            curve: Curves.fastOutSlowIn,
+            transform: Matrix4.translationValues(menuXPos, 0, 0),
+            child: ProfileSideMenu(menuWidth),
+          ),
+        ],
       ),
     );
   }
-
-
-
-  Row _appbar() {
-    return Row(
-      children: [
-        SizedBox(
-          width: 44,
-        ),
-        Expanded(
-            child: Text(
-          'The Coding Papa',
-          textAlign: TextAlign.center,
-        )),
-        IconButton(
-          icon: Icon(Icons.menu),
-          onPressed: () {},
-        ),
-      ],
-    );
-  }
 }
+
+enum MenuStatus { opened, closed }
--- lib/widget/profile_body.dart
+++ lib/widget/profile_body.dart
@@ -1,37 +1,184 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:instagram/constants/common_size.dart';
 import 'package:instagram/constants/screen_size.dart';
+import 'package:instagram/screens/profile_screen.dart';
+import 'package:instagram/widget/rounded_avatar.dart';
 
 class ProfileBody extends StatefulWidget {
+
+  final Function() onMenuChanged;
+
+  const ProfileBody({Key key, this.onMenuChanged}) : super(key: key);
+
   @override
   _ProfileBodyState createState() => _ProfileBodyState();
 }
 
-class _ProfileBodyState extends State<ProfileBody> {
+class _ProfileBodyState extends State<ProfileBody>
+    with SingleTickerProviderStateMixin {
+  //extends -  상속
+  //with - 상속과는 다르게 가지고와서 사용
+
   SelectedTab _selectedTab = SelectedTab.left;
+  double _leftImagesPageMargin = 0;
+  double _rightImagesPageMargin = size.width;
+  AnimationController _iconAnimationController;
+
+  @override
+  void initState() {
+    _iconAnimationController = AnimationController(vsync: this, duration: duration);
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _iconAnimationController.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Expanded(
-      child: CustomScrollView(
-        slivers: [
-          SliverList(
-            delegate: SliverChildListDelegate([
-              _username(),
-              _userBio(),
-              _editProfileBtn(),
-              _tabButtons(),
-              _selectedIndicator(),
-            ]),
+    return SafeArea(
+      // 상단 상태바 밑으로 body
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          _appbar(),
+          Expanded(
+            child: CustomScrollView(
+              slivers: [
+                SliverList(
+                  delegate: SliverChildListDelegate([
+                    Row(
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.all(common_gap),
+                          child: RoundedAvatar(
+                            size: 80,
+                          ),
+                        ),
+                        Expanded(
+                          child: Padding(
+                            padding: const EdgeInsets.only(right: common_gap),
+                            child: Table(
+                              children: [
+                                TableRow(children: [
+                                  _valueText('123124'),
+                                  _valueText('123123'),
+                                  _valueText('14242'),
+                                ]),
+                                TableRow(children: [
+                                  _labelText('Post'),
+                                  _labelText('Followers'),
+                                  _labelText('Following'),
+                                ]),
+                              ],
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                    _username(),
+                    _userBio(),
+                    _editProfileBtn(),
+                    _tabButtons(),
+                    _selectedIndicator(),
+                  ]),
+                ),
+                _imagesPager(),
+              ],
+            ),
           ),
         ],
       ),
     );
   }
 
+  Row _appbar() {
+    return Row(
+      children: [
+        SizedBox(
+          width: 44,
+        ),
+        Expanded(
+            child: Text(
+              'The Coding Papa',
+              textAlign: TextAlign.center,
+            )),
+        IconButton(
+          icon: AnimatedIcon(icon: AnimatedIcons.menu_close,
+            progress: _iconAnimationController,),
+          onPressed: () {
+            widget.onMenuChanged();
+            _iconAnimationController.status == AnimationStatus.completed
+                ? _iconAnimationController.reverse()
+                : _iconAnimationController.forward();
+          },
+        ),
+      ],
+    );
+  }
+
+  Text _valueText(String value) =>
+      Text(
+        value,
+        textAlign: TextAlign.center,
+        style: TextStyle(
+          fontWeight: FontWeight.bold,
+        ),
+      );
+
+  Text _labelText(String label) =>
+      Text(
+        label,
+        textAlign: TextAlign.center,
+        style: TextStyle(
+          fontWeight: FontWeight.w300,
+          fontSize: 11,
+        ),
+      );
+
+  SliverToBoxAdapter _imagesPager() {
+    return SliverToBoxAdapter(
+      child: Stack(
+        children: [
+          AnimatedContainer(
+            duration: duration,
+            transform: Matrix4.translationValues(_rightImagesPageMargin, 0, 0),
+            curve: Curves.fastOutSlowIn,
+            child: _images(),
+          ),
+          AnimatedContainer(
+            duration: duration,
+            transform: Matrix4.translationValues(_leftImagesPageMargin, 0, 0),
+            curve: Curves.fastOutSlowIn,
+            child: _images(),
+          ),
+        ],
+      ),
+    );
+  }
+
+  GridView _images() {
+    return GridView.count(
+      physics: NeverScrollableScrollPhysics(),
+      shrinkWrap: true,
+      crossAxisCount: 3,
+      childAspectRatio: 1,
+      children: List.generate(
+          30,
+              (index) =>
+              CachedNetworkImage(
+                fit: BoxFit.cover,
+                imageUrl: "https://picsum.photos/id/$index/100/100",
+              )),
+    );
+  }
+
   Widget _selectedIndicator() {
     return AnimatedContainer(
-      duration: Duration(milliseconds: 300),
+      duration: duration,
       alignment: _selectedTab == SelectedTab.left
           ? Alignment.centerLeft
           : Alignment.centerRight,
@@ -57,9 +204,7 @@ class _ProfileBodyState extends State<ProfileBody> {
                   : Colors.black26,
             ),
             onPressed: () {
-              setState(() {
-                _selectedTab = SelectedTab.left;
-              });
+              _tabSelected(SelectedTab.left);
             },
           ),
         ),
@@ -72,14 +217,29 @@ class _ProfileBodyState extends State<ProfileBody> {
                   : Colors.black,
             ),
             onPressed: () {
-              setState(() {
-                _selectedTab = SelectedTab.right;
-              });
+              _tabSelected(SelectedTab.right);
             },
           ),
         )
       ],
     );
+  }
+
+  _tabSelected(SelectedTab selectedTab) {
+    setState(() {
+      switch (selectedTab) {
+        case SelectedTab.left:
+          _selectedTab = SelectedTab.left;
+          _leftImagesPageMargin = 0;
+          _rightImagesPageMargin = size.width;
+          break;
+        case SelectedTab.right:
+          _selectedTab = SelectedTab.right;
+          _leftImagesPageMargin = -size.width;
+          _rightImagesPageMargin = 0;
+          break;
+      }
+    });
   }
 
   Padding _editProfileBtn() {
