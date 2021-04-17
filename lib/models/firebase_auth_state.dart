5ae7fc53967a858df50dcccb5a5d2ceb60b5109a
Chayoung Choi <chayoung.dev@gmail.com>
2021-04-17 23:48:31 +0900

    facebook login 연동

--- lib/models/firebase_auth_state.dart
+++ lib/models/firebase_auth_state.dart
@@ -2,6 +2,7 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_facebook_login/flutter_facebook_login.dart';
+import 'package:instagram/utils/simple_snackbar.dart';
 
 class FirebaseAuthState extends ChangeNotifier {
   FirebaseAuthStatus _firebaseAuthStatus = FirebaseAuthStatus.signout;
@@ -107,21 +108,35 @@ class FirebaseAuthState extends ChangeNotifier {
     final facebookLogin = FacebookLogin();
     final result = await facebookLogin.logIn(['email']);
 
-    switch(result.status){
-
+    switch (result.status) {
       case FacebookLoginStatus.loggedIn:
         _handleFacebookTokenWithFirebase(context, result.accessToken.token);
 
         break;
       case FacebookLoginStatus.cancelledByUser:
+        simpleSnackbar(context, 'User cancel facebook sign in');
         break;
       case FacebookLoginStatus.error:
+        simpleSnackbar(context, 'facebook sign in error');
+        facebookLogin.logOut();
         break;
     }
   }
 
-  void _handleFacebookTokenWithFirebase(BuildContext context, String token){
-    // TODO: 토큰을 사용해서 파이어베이스로 로그인하기
+  void _handleFacebookTokenWithFirebase(
+      BuildContext context, String token) async {
+    final AuthCredential credential =
+        FacebookAuthProvider.getCredential(accessToken: token);
+
+    final AuthResult authResult =
+        await _firebaseAuth.signInWithCredential(credential);
+    final FirebaseUser user = authResult.user;
+    if (user == null) {
+      simpleSnackbar(context, 'facebook 로그인에 실패하였습니다. 잠시 후 다시 시도해주세요.');
+    } else {
+      _firebaseUser = user;
+    }
+    notifyListeners();
   }
 
   FirebaseAuthStatus get firebaseAuthStatus => _firebaseAuthStatus;
